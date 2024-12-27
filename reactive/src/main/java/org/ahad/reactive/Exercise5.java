df992aae246ec166b0eabffc9c2a5baae731a2e0
Ahad Ali <linkonahad10@gmail.com>
2024-12-27 10:43:27 +0600

    Handling backpressure

--- reactive/src/main/java/org/ahad/reactive/Exercise5.java
+++ reactive/src/main/java/org/ahad/reactive/Exercise5.java
@@ -1,5 +1,8 @@
 package org.ahad.reactive;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -12,8 +15,28 @@ public class Exercise5 {
                 System.out::println,
                 () -> System.out.println("Complete")
         );
+
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
+
         System.out.println("Press a key to end");
         System.in.read();
+
     }
 
+}
+
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("On subscribe");
+        request(1);
+    }
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.println(value.toString() + " received");
+        request(1);
+    }
 }
