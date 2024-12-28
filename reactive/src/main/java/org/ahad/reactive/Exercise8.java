de20a3378f08c702524f60022a9cdc577737d0dc
Ahad Ali <linkonahad10@gmail.com>
2024-12-28 12:41:07 +0600

    Error handling

---
+++ reactive/src/main/java/org/ahad/reactive/Exercise8.java
@@ -0,0 +1,34 @@
+package org.ahad.reactive;
+
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+public class Exercise8 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFluxWithException()
+
+        // Print values from intNumbersFluxWithException and print a message when error happens
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(e -> System.out.println("Error: " + e.getMessage()))
+                .subscribe(System.out::println);
+
+        // Print values from intNumbersFluxWithException and continue on errors
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((e, o) -> System.out.println("Error: " + e.getMessage() + " for value " + o))
+                .subscribe(System.out::println);
+
+        // Print values from intNumbersFluxWithException and when errors
+        // happen, replace with a fallback sequence of -1 and -2
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume(e -> Flux.just(-1, -2))
+                .subscribe(System.out::println);
+
+        System.out.println("Press a key to end");
+        System.in.read();
+
+    }
+
+}
