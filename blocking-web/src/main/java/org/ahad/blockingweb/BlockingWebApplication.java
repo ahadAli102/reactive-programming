8eb9ddedddb79b710f46f7846e206852b610dc66
Ahad Ali <linkonahad10@gmail.com>
2024-12-29 12:49:19 +0600

    Blocking web application

---
+++ blocking-web/src/main/java/org/ahad/blockingweb/BlockingWebApplication.java
@@ -0,0 +1,13 @@
+package org.ahad.blockingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingWebApplication.class, args);
+    }
+
+}
---
+++ blocking-web/src/test/java/org/ahad/blockingweb/BlockingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahad.blockingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
